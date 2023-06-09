c28995641365af30656d02aa40a7fb9cfef51c26
gowtham <muragowtham@gmail.com>
2023-06-09 12:25:34 +0530

    selfish change

--- Single_Properties/src/test/java/com/Single_Properties/Runner_Face.java
+++ Single_Properties/src/test/java/com/Single_Properties/Runner_Face.java
@@ -29,7 +29,7 @@ public class Runner_Face {
 		
 		WebElement log = driver.findElement(By.xpath("//button[@name='login']"));
 		log.click();
-		System.out.println("you can change it");
+		System.out.println("change from selfish");
 		
 	}
 	
